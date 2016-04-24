d847a14bdf95fbc48d8ac5e79a479672a7f34fe2
Tao Liu <liut2@carleton.edu>
2016-04-24 17:28:42 -0500

    added error catch in unit test file

--- api_test.py
+++ api_test.py
@@ -24,7 +24,10 @@ class MovieQueryTester(unittest.TestCase):
 	def test_top_rated_typical(self):
 		url = "http://localhost:5000/topratedmovies/1"
 		data_from_server = requests.get(url).text
-		received_data = json.loads(data_from_server)
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
 		expected_data = [ { "rating": 4.44, "genres": [ "crime", "drama" ], "movie_id": 318, "release_year": 1994, "tmdb_id": 278, "title": "shawshank redemption, the ", "number_of_ratings": 77887, "imdb_id": 111161, "weighted_rating": 4.423 } ]
 		self.assertEqual(received_data,expected_data)
 		
@@ -32,7 +35,10 @@ class MovieQueryTester(unittest.TestCase):
 	def test_top_rated_empty(self):
 		url = "http://localhost:5000/topratedmovies/0"
 		data_from_server = requests.get(url).text
-		received_data = json.loads(data_from_server)
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
 		self.assertEqual(received_data,[])
 
 	def test_top_rated_null(self):
@@ -64,22 +70,31 @@ class MovieQueryTester(unittest.TestCase):
 	def test_favorite_from_year_typical1(self):
 		url = "http://localhost:5000/2015/toppicks/2"
 		data_from_server = requests.get(url).text
-		received_data = json.loads(data_from_server)
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
 		expected_data = [{"rating": 4.02, "genres": ["animation", "children", "comedy"], "movie_id": 134853, "release_year": 2015, "tmdb_id": 150540, "title": "inside out ", "number_of_ratings": 4360, "imdb_id": 2096673, "weighted_rating": 3.8481}, {"rating": 4.06, "genres": ["action", "adventure", "sci-fi"], "movie_id": 134130, "release_year": 2015, "tmdb_id": 286217, "title": "martian, the ", "number_of_ratings": 3277, "imdb_id": 3659388, "weighted_rating": 3.8353}]
 		self.assertEqual(received_data, expected_data)
 
 	def test_favorite_from_year_typical2(self):
 		url = "http://localhost:5000/1995/toppicks/1"
 		data_from_server = requests.get(url).text
-		received_data = json.loads(data_from_server)
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
 		expected_data = [{"rating": 4.32, "genres": ["crime", "mystery", "thriller"], "movie_id": 50, "release_year": 1995, "tmdb_id": 629, "title": "usual suspects, the ", "number_of_ratings": 53195, "imdb_id": 114814, "weighted_rating": 4.2975}]
 		self.assertEqual(received_data, expected_data)
 
 	def test_favorite_from_year_empty(self):
 		url = "http://localhost:5000/1995/toppicks/0"
 		data_from_server = requests.get(url).text
-		movie_list = json.loads(data_from_server)
-		self.assertEqual(movie_list,[])
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
+		self.assertEqual(received_data,[])
 
 	def test_favorite_from_year_null(self):
 		url = "http://localhost:5000/1995/toppicks/"
@@ -110,15 +125,21 @@ class MovieQueryTester(unittest.TestCase):
 	def test_recent_release_typical1(self):
 		url = "http://localhost:5000/recentrelease/1"
 		data_from_server = requests.get(url).text
-		received_data = json.loads(data_from_server)
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
 		expected_data = [{"rating": 3.75, "genres": ["action", "crime", "drama", "mystery", "thriller"], "movie_id": 150548, "release_year": 2016, "tmdb_id": 370646, "title": "sherlock: the abominable bride ", "number_of_ratings": 151, "imdb_id": 3845232, "weighted_rating": 3.1843}]
 		self.assertEqual(received_data, expected_data)
 
 	def test_recent_release_empty(self):
 		url = "http://localhost:5000/recentrelease/0"
 		data_from_server = requests.get(url).text
-		movie_list = json.loads(data_from_server)
-		self.assertEqual(movie_list, [])
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
+		self.assertEqual(received_data, [])
 
 	def test_recent_release_null(self):
 		url = "http://localhost:5000/recentrelease/"
@@ -139,22 +160,31 @@ class MovieQueryTester(unittest.TestCase):
 	def test_toprated_in_genre_typical1(self):
 		url = "http://localhost:5000/horror/2"
 		data_from_server = requests.get(url).text
-		received_data = json.loads(data_from_server)
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
 		expected_data = [{"rating": 4.16, "genres": ["crime", "horror", "thriller"], "movie_id": 593, "release_year": 1991, "tmdb_id": 274, "title": "silence of the lambs, the ", "number_of_ratings": 76271, "imdb_id": 102926, "weighted_rating": 4.1463}, {"rating": 4.07, "genres": ["crime", "horror"], "movie_id": 1219, "release_year": 1960, "tmdb_id": 539, "title": "psycho ", "number_of_ratings": 21198, "imdb_id": 54215, "weighted_rating": 4.0263}]
 		self.assertEqual(received_data, expected_data)
 
 	def test_toprated_in_genre_upper_case_genre(self):
 		url = "http://localhost:5000/Animation/1"
 		data_from_server = requests.get(url).text
-		received_data = json.loads(data_from_server)
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
 		expected_data = [{"rating": 4.19, "genres": ["adventure", "animation", "fantasy"], "movie_id": 5618, "release_year": 2001, "tmdb_id": 129, "title": "spirited away (sen to chihiro no kamikakushi) ", "number_of_ratings": 16830, "imdb_id": 245429, "weighted_rating": 4.1288}]
 		self.assertEqual(received_data, expected_data)
 
 	def test_toprated_in_genre_empty(self):
 		url = "http://localhost:5000/drama/0"
 		data_from_server = requests.get(url).text
-		movie_list = json.loads(data_from_server)
-		self.assertEqual(movie_list, [])
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
+		self.assertEqual(received_data, [])
 
 	def test_toprated_in_genre_invalid_null_number(self):
 		url = "http://localhost:5000/drama/"
@@ -180,40 +210,58 @@ class MovieQueryTester(unittest.TestCase):
 	def test_title_contains_typical1(self):
 		url = "http://localhost:5000/titlecontains?query=the+revenant"
 		data_from_server = requests.get(url).text
-		received_data = json.loads(data_from_server)
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
 		expected_data = [{"rating": 3.93, "genres": ["adventure", "drama"], "movie_id": 139385, "release_year": 2015, "tmdb_id": 281957, "title": "the revenant ", "number_of_ratings": 446, "imdb_id": 1663202, "weighted_rating": 3.3552}]
 		self.assertEqual(received_data, expected_data)
 
 	def test_title_contains_typical3(self):
 		url = "http://localhost:5000/titlecontains?query=avatar"
 		data_from_server = requests.get(url).text
-		received_data = json.loads(data_from_server)
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
 		expected_data = [{"rating": 3.73, "genres": ["action", "adventure", "sci-fi", "imax"], "movie_id": 72998, "release_year": 2009, "tmdb_id": 19995, "title": "avatar ", "number_of_ratings": 14555, "imdb_id": 499549, "weighted_rating": 3.6894}]
 		self.assertEqual(received_data, expected_data)
 
 	def test_title_contains_not_valid_title(self):
 		url = "http://localhost:5000/titlecontains?query=sfdjlkwejrwlerk1213r"
 		data_from_server = requests.get(url).text
-		movie_list = json.loads(data_from_server)
-		self.assertEqual(movie_list, [])
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
+		self.assertEqual(received_data, [])
 
 	def test_title_contains_invalid1(self):
 		url = "http://localhost:5000/titlecontains?"
 		data_from_server = requests.get(url).text
-		movie_list = json.loads(data_from_server)
-		self.assertEqual(movie_list, [])
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
+		self.assertEqual(received_data, [])
 
 	def test_title_contains_invalid2(self):
 		url = "http://localhost:5000/titlecontains"
 		data_from_server = requests.get(url).text
-		movie_list = json.loads(data_from_server)
-		self.assertEqual(movie_list, [])
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
+		self.assertEqual(received_data, [])
 
 	def test_title_contains_invalid3(self):
 		url = "http://localhost:5000/titlecontains?dswe"
 		data_from_server = requests.get(url).text
-		movie_list = json.loads(data_from_server)
-		self.assertEqual(movie_list, [])
+		try:
+			received_data = json.loads(data_from_server)
+		except ValueError:
+			received_data = None
+		self.assertEqual(received_data, [])
 
 
 if __name__ == '__main__':
--- movie_hub.py
+++ movie_hub.py
@@ -10,6 +10,7 @@ query = Query()
 @app.route('/')
 def hello():
     return "meow meow"
+	
 
 @app.errorhandler(404)
 def not_found_error_handler(e):
@@ -21,18 +22,21 @@ def get_toprated(first_n):
     if first_n >= 0 and first_n < 10000:
 		return query.get_toprated(first_n)
     return "Please enter a valid url for toprated movies query."
+	
 
 @app.route('/<int:year>/toppicks/<int:first_n>')
 def get_favourite_from_year(year, first_n):
     if year >= 1970 and year <= 2016 and first_n >= 0 and first_n < 10000:
 		return query.get_favourite_from_year(year, first_n)
     return "Please enter a valid url for get favourite movies from year query."
+	
 
 @app.route('/recentrelease/<int:first_n>')
 def get_recent_release(first_n):
     if first_n >= 0 and first_n < 10000:
 		return query.get_recent_release(first_n)
     return "Please enter a valid url for recent release query."
+	
 
 #we also need to check if the imput genre is in the genre list
 #this also need to be tested in unittest
@@ -43,7 +47,7 @@ def get_toprated_in_genre(genre, first_n):
 	if first_n >= 0 and first_n < 10000 and (genre in genre_set):
 		return query.get_toprated_in_genre(genre, first_n)
 	return "Please enter a valid url for get toprated in genre query."
-
+	
 #here the query doesn't support multiple string like "the+star+war" yet,
 #will add this functionality later, this also need to be tested in unittest
 #also in flask the query paramter doesn't show up in url, we need to retrieve it
