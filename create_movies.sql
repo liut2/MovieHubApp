8d8bc68b1190f037aaa6f980ad39f65253a04753
liut2 <liut2@cmc307-02.mathcs.carleton.edu>
2016-05-01 15:45:27 -0500

    newer version of movie data, combined imdb

--- create_movies.sql
+++ create_movies.sql
@@ -1,6 +1,6 @@
 create table movies
 	(movie_id	int,
-	 title		varchar(500),
+	 title		varchar(200),
 	 genres		varchar(100)[],
 	 imdb_id	int,
 	 tmdb_id	int,
@@ -8,5 +8,9 @@ create table movies
 	 number_of_ratings	int,
 	 weighted	float(1),
 	 release_year	int,
+	 img_path	varchar(300),
+	 description varchar(600),
+	 director	varchar(100),
+	 length	varchar(20),
 	 primary key(movie_id));
-	 #app.run(host='http://thacker.mathcs.carleton.edu', port=5128)
+	 
