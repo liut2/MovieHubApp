9c6e496eb4e1c9ddfa0cd0caf399a5b09f857c96
Xi Chen <chenx2@carleton.edu>
2016-05-07 15:56:26 -0500

    a

--- WebSite/static/js/modal.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$("#signup-modal").click(function(){
-        $("#modal1").openModal();
-    });
-
-    $("#loginbutton").click(function(){
-    	$("#modal1").closeModal();
-    });
-});
