a089bef2f005ddad806584a8a78a619026fb8c13
Xi Chen <chenx2@carleton.edu>
2016-05-10 10:48:57 -0500

    added pagination class

--- website-newest/app.py
+++ website-newest/app.py
@@ -3,19 +3,20 @@ from flask_oauth import OAuth
 from user_query import UserQuery
 from api_query import MovieQuery
 import json
+import random
+import config
 from Pagination import Pagination
-SECRET_KEY = 'moviehub development key'
-FACEBOOK_APP_ID = '260838524257280'
-FACEBOOK_APP_SECRET = 'a0210b8252d9590f8bd7c1bbb645782c'
-PER_PAGE = 15
 
 app = Flask(__name__)
+
+#import Facebook login setup from config.py
+SECRET_KEY = config.SECRET_KEY
+FACEBOOK_APP_ID = config.FACEBOOK_APP_ID
+FACEBOOK_APP_SECRET = config.FACEBOOK_APP_SECRET
 app.secret_key = SECRET_KEY
 oauth = OAuth()
-userquery = UserQuery()
-moviequery = MovieQuery()
-query = MovieQuery()
 
+#init Facebook login setup
 facebook = oauth.remote_app('facebook',
 	base_url='https://graph.facebook.com/',
 	request_token_url=None,
@@ -26,100 +27,93 @@ facebook = oauth.remote_app('facebook',
 	request_token_params={'scope': 'email'}
 )
 
-@app.route("/")
+#declaration of other variables
+userquery = UserQuery()
+moviequery = MovieQuery()
+LIMIT_PER_PAGE = config.LIMIT_PER_PAGE
+genre_list = []
+movie_preference = []
+genre_preference = []
+
+
+'''This controller handles root route and renders home page for our web app.'''
+@app.route("/",methods=['POST', 'GET'])
 def index():
-	genres = request.args.get('genres')
-	movies = request.args.get('movies')
-	genre_list = []
-	toprated_in_genre = {}
-
-	if genres != None:
-		genres = json.loads(genres)
-	
-	if movies != None:
-		movies = json.loads(movies)
-	if genres != None and movies != None:
+	global genre_list
+	# Receiving Ajax POST request means user has updated their preference, then we update the info 
+	# in database and reload our page to show that update.
+	if request.method == "POST":
+		response = request.json
 		session["alread_choose_preference"] = True
-		userquery.update_user_preference(int(session["user_id"]), genres, movies)
+		genres = response["selected_genres"]
+		movies = response["selected_movies"]
+		update_user_preference(genres, movies)
+		return "success"
 
+	#else if the request is GET, we simply render the template as we normally do.
 	toprated = json.loads(moviequery.get_toprated_for_page(1,30))
 	favourite_last_year = json.loads(moviequery.get_favourite_from_year_for_page(2015, 1,30))
 	recent_release = json.loads(moviequery.get_favourite_from_year_for_page(2016,1,30))
-	freq = [1, 2, 3, 4, 5]
+	locked_content_count = [1, 2, 3, 4, 5]
+	init_preference_chosen_list()
 	
-	if "logged_in" in session and session["logged_in"]:
-		user_obj = json.loads(userquery.find_user_by_id(int(session["user_id"])))
-		genres = user_obj['selected_genres']
-		movies = user_obj['selected_movies']
-		for gen in genres:
-			gen = gen.lower()
-			toprated_in_genre["type"] = gen
-			toprated_in_genre["body"] = json.loads(moviequery.get_toprated_in_genre(gen, 5))
-			genre_list.append(toprated_in_genre)
-			toprated_in_genre = {}
-	return render_template("index.html", toprated = toprated, lastyear = favourite_last_year, recent = recent_release, freq = freq, genre_list = genre_list)
+	return render_template("index.html", toprated = toprated, lastyear = favourite_last_year, recent = recent_release, freq = locked_content_count, genre_list = genre_list, genre_preference = genre_preference, movie_preference = movie_preference)
 
-@app.route("/search",defaults={'page': 1})
-@app.route("/search/page/<int:page>")
-def search(page):
+'''This controller handles the route for search page, and renders the search results.'''
+@app.route("/search")
+def search():
 	empty = True
 	string = request.args.get("query").lower()
 	search_result = json.loads(moviequery.get_movies_containing_title(string))
-	pagination = Pagination(page, PER_PAGE, 1)
+	pagination = Pagination(1, LIMIT_PER_PAGE, 1)
 	if(not search_result):
 		empty = False
 		return render_template("search_page.html",type = "search",word=string+" not found",search_result = search_result,pagination = pagination,empty = empty)
 	else:
 		return render_template("search_page.html",type = "search",word=string,search_result = search_result,pagination = pagination,empty = empty)
 
-@app.route("/toprated",defaults={'page': 1})
-@app.route('/toprated/page/<int:page>')
-def toprated(page):
+'''This controller handles the route for rendering details for a specific movie channel.'''
+@app.route("/<type>",defaults={'page': 1})
+@app.route("/<type>/page/<int:page>")
+def seemore(type,page):
 	empty = True
-	toprated = json.loads(moviequery.get_toprated_for_page(page,15))
-	if (not toprated and page !=1):
-		return render_template("404.html")
-	else:
+	if type == "toprated":
+		toprated = json.loads(moviequery.get_toprated_for_page(page, LIMIT_PER_PAGE))
 		count = moviequery.get_toprated_with_count()
-		pagination = Pagination(page, PER_PAGE, count)
+		pagination = Pagination(page, LIMIT_PER_PAGE, count)
 		return render_template("search_page.html",type = "toprated",word="Top Rated",search_result = toprated, pagination = pagination,empty = empty)
-
-@app.route("/recentrelease",defaults={'page': 1})
-@app.route('/recentrelease/page/<int:page>')
-def recentrelease(page):
-	empty = True
-	recentrelease = json.loads(moviequery.get_favourite_from_year_for_page(2016,page,15))
-	if (not recentrelease and page !=1):
-		return render_template("404.html")
-	else:
-		count = moviequery.get_recent_release_with_count()
-		pagination = Pagination(page, PER_PAGE, count)
-		return render_template("search_page.html",type = "recentrelease",word="Recent release",search_result = recentrelease,pagination=pagination,empty = empty)
-
-@app.route("/favoritelastyear",defaults={'page': 1})
-@app.route('/favoritelastyear/page/<int:page>')
-def favoritelastyear(page):
-	empty = True
-	favoritelastyear = json.loads(moviequery.get_favourite_from_year_for_page(2015,page,15))
-	if (not favoritelastyear and page !=1):
-		return render_template("404.html")
-	else:
+	elif type == "recentrelease":
+		recentrelease = json.loads(moviequery.get_favourite_from_year_for_page(2016, page, LIMIT_PER_PAGE))
+		count = moviequery.get_favourite_from_year_count(2016)
+		pagination = Pagination(page, LIMIT_PER_PAGE, count)
+		return render_template("search_page.html",type = "recentrelease",word="Recent Release",search_result = recentrelease, pagination = pagination,empty = empty)
+	elif type == "favoritelastyear":
+		favoritefromlastyear = json.loads(moviequery.get_favourite_from_year_for_page(2015, page, LIMIT_PER_PAGE))
 		count = moviequery.get_favourite_from_year_count(2015)
-		pagination = Pagination(page, PER_PAGE, count)
-		return render_template("search_page.html",type = "favoritelastyear",word="Favorite from Last Year",search_result = favoritelastyear,pagination= pagination,empty = empty)
-
-
+		pagination = Pagination(page, LIMIT_PER_PAGE, count)
+		return render_template("search_page.html",type = "favoritelastyear",word="Favorite From Last Year",search_result = favoritefromlastyear, pagination = pagination,empty = empty)
+	else:
+		genre = json.loads(moviequery.get_toprated_in_genre_for_page(type.lower(), page, LIMIT_PER_PAGE))
+		count = moviequery.get_toprated_in_genre_with_count(type.lower())
+		pagination = Pagination(page, LIMIT_PER_PAGE, count)
+		return render_template("search_page.html",type = type,word=type,search_result = genre, pagination = pagination,empty = empty)
+	
+'''This controller handles the route for login page, which redirects user
+to Facebook login.'''
 @app.route('/login')
 def login():
 	return facebook.authorize(callback=url_for('facebook_authorized',
 		next=request.args.get('next') or request.referrer or None,
 		_external=True))
 
+'''This controller handles the route for logout page, which redirects user back to home page before login.'''
 @app.route("/logout")
 def logout():
+	#clear user session when logout
 	pop_login_session()
 	return redirect(url_for('index'))
 
+'''This controller handles the callback after user has successfully logged in with Facebook.'''
 @app.route('/login/authorized')
 @facebook.authorized_handler
 def facebook_authorized(resp):
@@ -129,7 +123,7 @@ def facebook_authorized(resp):
 	session['oauth_token'] = (resp['access_token'], '')
 	session['logged_in'] = True
 
-
+	#update user info in session
 	me = facebook.get('/me')
 	user_id = int(me.data['id'])
 	user_name = me.data['name']
@@ -138,13 +132,14 @@ def facebook_authorized(resp):
 
 	if userquery.check_if_user_exist(user_id):
 		session["alread_choose_preference"] = True
-		print "alread_signup"
 	else:
 		userquery.add_user(user_id, user_name)
 		session["alread_choose_preference"] = False
-		print "not_signup"
 	
 	return redirect(url_for('index'))
+
+'''This section contains some helper methods that are used to construct 
+functions above.'''
 
 @facebook.tokengetter
 def get_facebook_token():
@@ -155,7 +150,45 @@ def pop_login_session():
 	session.pop('oauth_token', None)
 	session.pop('user_id', None)
 	session.pop('user_name', None)
-	#session["alread_signup"] = None
+
+def shuffle(arr):
+	for i in range(len(arr)):
+		r = random.randrange(len(arr))
+		temp = arr[i]
+		arr[i] = arr[r]
+		arr[r] = temp
+	return arr
+
+def update_user_preference(genres, movies):
+	userquery.update_user_preference(int(session["user_id"]), genres, movies)
+	global genre_list
+	toprated_in_genre = {}
+	for gen in genres:
+		gen = gen.lower()
+		toprated_in_genre["type"] = gen
+		toprated_in_genre["body"] = json.loads(moviequery.get_toprated_in_genre_for_page(gen, 1,30))
+		genre_list.append(toprated_in_genre)
+		toprated_in_genre = {}
+
+def init_preference_chosen_list():
+	global genre_preference
+	global movie_preference
+	genre_preference = []
+	movie_preference = []
+	#generate genre preference list
+	genre_preference = ['mystery', 'romance', 'sci-fi', 'horror', 'children', 'film-noir', 'crime', 'drama', 'fantasy', 'animation', 'adventure', 'western', 'action', 'musical', 'comedy', 'documentary', 'war', 'thriller', 'imax']
+	#generate movie preference list
+	movie_set = set()
+	for gen in genre_preference:
+		rows = json.loads(moviequery.get_toprated_in_genre_for_page(gen, 1,30))
+		for i in range(len(rows)):
+			title = rows[i]["title"]
+			if len(title) < 20 and (title not in movie_set) and ("'" not in title):
+				movie_preference.append(rows[i])
+				movie_set.add(title)
+	#randomize the order
+	genre_preference = shuffle(genre_preference)
+	movie_preference =  shuffle(movie_preference)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -198,4 +231,4 @@ def get_movies_containing_title():
 	return query.get_movies_containing_title(string)
 
 if __name__ == "__main__":
-	app.run(debug=True, host="localhost", port=5000)
+	app.run(debug=True, host = config.host, port = config.port)
